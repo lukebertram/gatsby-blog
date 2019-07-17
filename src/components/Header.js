5717e0a75b4a679d13a596c4c015b711db1a0d1f
Luke <luke@Johnnys-iMac.local>
2019-07-17 16:34:40 -0700

    add gatsby-node.js config for generating pages from markdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StaticQuery } from 'gatsby'
+import { StaticQuery, graphql } from 'gatsby'
 import TitleAndDescription from './TitleAndDescription'
 
 const Header = () => {
