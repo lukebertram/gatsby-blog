4a451c5b6736e9d74f75173bd59b5d2fa787c575
Luke <luke@Johnnys-iMac.local>
2019-07-18 19:37:59 -0700

    add links for prev and next posts to blogTemplate

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,12 +1,52 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 
-const Template = (props) => {
+const Template = ({data, pageContext}) => {
+	const {next, prev} = pageContext
+	const {markdownRemark} = data
+	const title = markdownRemark.frontmatter.title
+	const html = markdownRemark.html
+
 	return (
 		<div>
-			Blog Post Here
+			<h1 style={{fontFamily: 'avenir'}}>{title}</h1>
+			<div className='blogpost'
+				dangerouslySetInnerHTML={{__html: html}}
+				style={{
+					fontFamily: 'avenir'
+				}}
+			/>
+			<div style={{
+				display: 'flex',
+				flexDirection: 'row',
+				justifyContent: 'space-around',
+				alignItems: 'center',
+				fontFamily: 'avenir'
+			}}>
+				{ prev &&
+					<Link to={prev.frontmatter.path}>
+						Previous
+					</Link>
+				}
+				{ next && 
+					<Link to={next.frontmatter.path}>
+						Next
+					</Link>
+				}
+			</div>
 		</div>
 	)
 }
+
+export const query = graphql`
+	query($pathSlug: String!) {
+		markdownRemark(frontmatter: {path: {eq: $pathSlug} }) {
+			html
+			frontmatter {
+				title
+			}
+		}
+	}
+`
 
 export default Template
